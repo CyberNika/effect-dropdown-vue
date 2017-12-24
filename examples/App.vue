1e0e968a86936dc1958c7b226aea41f8045d6260
XBT1 <xbtop1@gmail.com>
2017-12-24 09:43:12 +0800

    feat: first blood

---
+++ examples/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>
+        effect-dropdown
+        <iframe
+          class="gh-star"
+          scrolling="0"
+          width="170px"
+          height="20px"
+          src="https://ghbtns.com/github-btn.html?user=XBT1&repo=effect-dropdown&type=star&count=true" frameborder="0" >
+        </iframe>
+      </h1>
+
+      <p>
+        <span class="description">Simple effects for Dropdown based on Vue.js</span>
+      </p>
+    </header>
+
+    <main>
+      <effect-dropdown label="Label">
+        <effect-dropdown-item>Money</effect-dropdown-item>
+        <effect-dropdown-item>Bear</effect-dropdown-item>
+      </effect-dropdown>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    data () {
+      return {}
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  #app
+    max-width: 1000px
+    margin: 0 auto
+</style>
+
+<style lang="stylus">
+  html
+    height: 100%
+
+  body
+    height: 100%
+    color: #333
+    font-size: 16px
+    line-height: 1.5
+    background: url("https://tympanus.net/Development/SimpleDropDownEffects/images/grid_DominikKiss_SubtlePatterns.jpg")
+</style>
---
+++ src/components/EffectDropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="effect-dropdown">
+    <span class="effect-dropdown__label">
+      {{ label }}
+    </span>
+
+    <ul class="effect-dropdown__content">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+  const TYPE_LIST = ['drawer', 'fence']
+  const EFFECT_LIST = ['randomAngle', 'arc']
+
+  export default {
+    name: 'EffectDropdown',
+
+    props: {
+      type: {
+        default: 'drawer',
+        validator (value) {
+          return typeof value === 'string' && TYPE_LIST.indexOf(value) > -1
+        },
+      },
+      EFFECT_LIST: {
+        default: null,
+        validator (value) {
+          return !value || (typeof value === 'string' && TYPE_LIST.indexOf(value) > -1)
+        },
+      },
+      label: String,
+      gutter: {
+        type: [Number, String],
+        default: 5,
+      },
+      activeColor: {
+        type: String,
+        default: '#fc756f',
+      },
+      raiseLabel: Boolean,
+    },
+  }
+</script>
