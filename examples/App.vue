162c709ece8c058ecd1f0907b217ef38034c75c7
snokier <me@snokier.com>
2017-12-26 00:28:41 +0800

    feat: 添加不同效果

--- examples/App.vue
+++ examples/App.vue
@@ -18,12 +18,66 @@
     </header>
 
     <main>
-      <effect-dropdown label="Label">
-        <effect-dropdown-item>Money</effect-dropdown-item>
-        <effect-dropdown-item>Bear</effect-dropdown-item>
-        <effect-dropdown-item>Money1</effect-dropdown-item>
-        <effect-dropdown-item>Bear1</effect-dropdown-item>
-      </effect-dropdown>
+      <div class="dropdown-wrapper">
+        <p class="dropdown-desc">
+          默认效果，折叠时具有些许间隔，激活时选项之间间隔 5px
+        </p>
+
+        <div class="dropdown-main">
+          <effect-dropdown label="Label">
+            <effect-dropdown-item>Money</effect-dropdown-item>
+            <effect-dropdown-item>Bear</effect-dropdown-item>
+            <effect-dropdown-item>Money1</effect-dropdown-item>
+            <effect-dropdown-item>Bear1</effect-dropdown-item>
+          </effect-dropdown>
+        </div>
+      </div>
+
+      <div class="dropdown-wrapper">
+        <p class="dropdown-desc">
+          randomAngle 效果，折叠时具有些许间隔，激活时选项之间间隔 5px，每个选项具有随机的角度旋转和偏移，以及不同的 animation-delay
+        </p>
+
+        <div class="dropdown-main">
+          <effect-dropdown label="Label" active-color="#4d8c9d" effect="randomAngle">
+            <effect-dropdown-item>Money</effect-dropdown-item>
+            <effect-dropdown-item>Bear</effect-dropdown-item>
+            <effect-dropdown-item>Money1</effect-dropdown-item>
+            <effect-dropdown-item>Bear1</effect-dropdown-item>
+          </effect-dropdown>
+        </div>
+      </div>
+
+      <div class="dropdown-wrapper">
+        <p class="dropdown-desc">
+          camber 效果，折叠时具有些许间隔，激活时选项之间间隔 5px，依次排列组成圆弧状，并有不同的 animation-delay
+        </p>
+
+        <div class="dropdown-main">
+          <effect-dropdown label="Label" active-color="#97d1bc" effect="camber">
+            <effect-dropdown-item>Money</effect-dropdown-item>
+            <effect-dropdown-item>Bear</effect-dropdown-item>
+            <effect-dropdown-item>Money1</effect-dropdown-item>
+            <effect-dropdown-item>Bear1</effect-dropdown-item>
+          </effect-dropdown>
+        </div>
+      </div>
+
+
+      <div class="dropdown-wrapper">
+        <p class="dropdown-desc">
+          raiseLabel 效果，折叠时具有些许间隔，激活时选项之间没有间隔，依次排列。label 被点击时会有收缩的 3D 效果。
+        </p>
+
+        <div class="dropdown-main">
+          <effect-dropdown label="Label" active-color="#f8b161" raise-label>
+            <effect-dropdown-item>Money</effect-dropdown-item>
+            <effect-dropdown-item>Bear</effect-dropdown-item>
+            <effect-dropdown-item>Money1</effect-dropdown-item>
+            <effect-dropdown-item>Bear1</effect-dropdown-item>
+          </effect-dropdown>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -39,9 +93,42 @@
 </script>
 
 <style lang="stylus" scoped>
+  dropdown-wrapper-height = 150px
+
   #app
     max-width: 1000px
     margin: 0 auto
+
+  main
+    padding: 30px 0 60px
+
+  .dropdown-wrapper
+    padding: 10px
+    margin: 20px
+    height: dropdown-wrapper-height
+
+    &:nth-child(odd)
+      .dropdown-desc
+        float: left
+        padding-right: 25px
+
+    &:nth-child(even)
+      .dropdown-desc
+        float: right
+        padding-left: 25px
+
+    .dropdown-main
+      text-align: center
+
+    .dropdown-desc
+      padding-top: 5px
+      margin: 0
+      width: 50%
+      color: #aaa
+      font-size: 18px
+      line-height: 1.2
+      font-weight: 300
+      text-shadow: 0 1px 1px rgba(255, 255, 255, 0.8)
 </style>
 
 <style lang="stylus">
--- src/components/EffectDropdownItem.vue
+++ src/components/EffectDropdownItem.vue
@@ -3,7 +3,7 @@
     v-if="index !== null"
     :style="itemStyle"
     @click="handleClick"
-    class="effect-dropdown-item"
+    :class="classname"
     @mouseenter="handleMouseenter"
     @mouseleave="handleMouseleave">
     <slot>
@@ -15,7 +15,7 @@
 <script>
   import { transform } from '../utils'
 
-  const base = {
+  const BASE = {
     zIndex: 1500,
     offset: 3,
     itemHeight: 60,
@@ -41,32 +41,88 @@
       active () {
         return this.$$root.active
       },
+      classname () {
+        return {
+          'effect-dropdown-item': true,
+          [`effect-dropdown-item--${this.$$root.effect}`]: true,
+          'effect-dropdown-item--active': this.active,
+        }
+      },
 
       transform () {
+        const { effect } = this.$$root
         const scale = 1 - (this.index / 100)
 
-        const defaultTransform = transform({
-          scale3d: [scale, scale, 1],
-        })
+        const commonOptions = {
+          default: { scale3d: [scale, scale, 1] },
+          active: { scale3d: [1, 1, 1] },
+        }
+        const optionsMap = {
+          simple: { ...commonOptions },
+          randomAngle: {
+            default: {
+              ...commonOptions.default,
+              rotate: 0,
+            },
+            active: {
+              ...commonOptions.active,
+              rotate: `${(Math.random() * 10 - 5).toFixed(2)}deg`,
+            },
+          },
+          camber: {
+            default: {
+              ...commonOptions.default,
+              rotate: 0,
+            },
+            active: {
+              ...commonOptions.active,
+              rotate: `${this.index * 5}deg`,
+            },
+          },
+        }
 
-        const activeTransfrom = transform({
-          scale3d: [1, 1, 1],
-        })
+        const defaultTransform = transform(optionsMap[effect].default)
+
+        const activeTransfrom = transform(optionsMap[effect].active)
 
         return this.active ? activeTransfrom : defaultTransform
       },
 
       itemStyle () {
+        const { effect, items } = this.$$root
         const top = this.active ?
-          `${this.index * (base.itemHeight + this.$$root.gutter)}px` :
-          `${base.offset * this.index}px`
+          `${(this.index + 1) * (BASE.itemHeight + this.$$root.gutter)}px` :
+          `${BASE.offset * this.index}px`
 
-        return {
+        const commonStyle = {
           top,
           color: this.hover ? '#fff': null,
           backgroundColor: this.hover ? this.$$root.activeColor: null,
           transform: this.transform,
-          zIndex: base.zIndex - (this.index + 1),
+          zIndex: BASE.zIndex - (this.index + 1),
+        }
+
+        const styleMap = {
+          simple: {},
+          randomAngle: {
+            left: this.active ? `${Math.floor(Math.random() * 10 - 5)}px` : 0,
+            'transition-delay': this.active ?
+              `${(items.length - this.index - 1) * 100}ms` :
+              `${this.index * 100}ms`
+          },
+          camber: {
+            top: this.active ?
+              `${(BASE.itemHeight + this.$$root.gutter)}px` :
+              commonStyle.top,
+            'transition-delay': this.active ?
+              `${(items.length - this.index - 1) * 50}ms` :
+              `${this.index * 50}ms`,
+          },
+        }
+
+        return {
+          ...commonStyle,
+          ...styleMap[effect],
         }
       },
     },
@@ -80,6 +136,7 @@
     methods: {
       handleMouseenter (event) {
         if (this.disabled) return
+        if (!this.active) return
 
         this.hover = true
 
